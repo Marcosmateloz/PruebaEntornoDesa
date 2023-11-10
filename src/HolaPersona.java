5635e3b96eac70bda6de9119077a8d96d2ccac4b
marcos <mmateosl02@educarex.es>
2023-11-10 12:23:29 +0100

    Saludo Persona

---
+++ src/HolaPersona.java
@@ -0,0 +1,16 @@
+import javax.swing.JOptionPane;
+
+public class HolaPersona {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String nombre= JOptionPane.showInputDialog("Dame tu nombre");
+		
+		JOptionPane.showInputDialog(null, "Hola " + nombre);
+		
+		
+		
+	}
+
+}
